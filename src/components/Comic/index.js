77fbeb51f590ffcbdd51f09931173772356d45bd
Beatrice Jin <beatricezjin@gmail.com>
2020-12-14 23:49:30 -0500

    some artisan crap

--- src/components/Comic/index.js
+++ src/components/Comic/index.js
@@ -1,8 +1,21 @@
-import './styles.scss';
+import React from 'react';
+import { useLayoutBreakpoints } from 'ai2jsx';
 
-const Comic = props => {
+import Artboard_1 from './Artboard_1';
+
+const LAYOUTS = {
+  Artboard_1: {
+    breakpoint: 1200,
+    component: Artboard_1,
+  },
+};
+
+const Comic = () => {
+  const layout = useLayoutBreakpoints(LAYOUTS);
+
   return (
-    <div className='comic'>
+    <div className='ai-artisan-file'>
+      {layout}
     </div>
   );
 };
