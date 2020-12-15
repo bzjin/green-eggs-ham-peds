ba3835cf1c16706adfee3e9a47500908303ef9a8
Beatrice Jin <beatricezjin@gmail.com>
2020-12-15 13:54:52 -0500

    mobile

--- src/components/Comic/index.js
+++ src/components/Comic/index.js
@@ -1,10 +1,38 @@
-import './styles.scss';
+import React from 'react';
+import { useLayoutBreakpoints } from 'ai2jsx';
 
-const Comic = props => {
+import Desktop from './Desktop';
+import Mobile from './Mobile';
+import Ipad from './Ipad';
+import XSMobile from './XSMobile';
+
+const LAYOUTS = {
+  Desktop: {
+    breakpoint: 800,
+    component: Desktop,
+  },
+  Mobile: {
+    breakpoint: 450,
+    component: Mobile,
+  },
+  Ipad: {
+    breakpoint: 600,
+    component: Ipad,
+  },
+  XSMobile: {
+    breakpoint: 290,
+    component: XSMobile,
+  },
+};
+
+const Panel1 = () => {
+  const layout = useLayoutBreakpoints(LAYOUTS);
+
   return (
-    <div className='comic'>
+    <div className='ai-panel1'>
+      {layout}
     </div>
   );
 };
 
-export default Comic;
+export default Panel1;
