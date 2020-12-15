e605577241b3ccc176b5ec13d471324f5af54e0f
Beatrice Jin <beatricezjin@gmail.com>
2020-12-15 11:45:29 -0500

    desktop

--- src/components/Comic/index.js
+++ src/components/Comic/index.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import { useLayoutBreakpoints } from 'ai2jsx';
 
-import Artboard_1 from './Artboard_1';
+import Desktop from './Desktop';
+import Artboard_6 from './Artboard_6';
 
 const LAYOUTS = {
-  Artboard_1: {
-    breakpoint: 1200,
-    component: Artboard_1,
+  Desktop: {
+    breakpoint: 800,
+    component: Desktop,
+  },
+  Artboard_6: {
+    breakpoint: 450,
+    component: Artboard_6,
   },
 };
 
-const Comic = () => {
+const Panel1 = () => {
   const layout = useLayoutBreakpoints(LAYOUTS);
 
   return (
-    <div className='ai-artisan-file'>
+    <div className='ai-panel1'>
       {layout}
     </div>
   );
 };
 
-export default Comic;
+export default Panel1;
